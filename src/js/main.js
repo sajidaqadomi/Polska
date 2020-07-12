ed6ee1174758de481237472b81a34f99af4d9493
Sajida Qadomi <sajidaqadomi@outlook.sa>
2020-07-12 11:55:19 +0300

    add product section

--- src/js/main.js
+++ src/js/main.js
@@ -5,21 +5,27 @@ $(function () {
         upperHead = $('.upper-header'),
         hamburgerIcon = $('.hamburger-icon'),
         navLink = $('.lower-header .nav-item .nav-link'),
-        // searchInput = $('.search-area .search'),
         searchIcon = $('.link-search')
     menuLink = $('#menu .nav-list a'),
-        menu = $('#menu');
+        menu = $('#menu'),
+        outLContainer = $('.out-lcontainer'),
+        //////////////
+        homeProducts = $('.home-products-li'),
+        productSelect = $('.product-select');
+
     ///**********Dimentions********/
     let headerHeigh, lowHeadHeigh, upperHeadHeigh, menuWidth;
 
     function calcDimetions() {
-        headerHeigh = headerContent.innerHeight(),
-            lowHeadHeigh = lowHead.innerHeight(),
-            upperHeadHeigh = upperHead.innerHeight(),
-            menuWidth = menu.innerWidth();
+        headerHeigh = headerContent.innerHeight();
+        lowHeadHeigh = lowHead.innerHeight();
+        upperHeadHeigh = upperHead.innerHeight();
+        menuWidth = menu.innerWidth();
+
+
     }
 
-    //************header**********/
+    //************header******************************************************************************/
     //************search icon**********/
     //
     function controlSearchicon(icon) {
@@ -57,8 +63,8 @@ $(function () {
         );
 
     }
-    function setTopMenu() {
-        //lowHead.hasClass('sticky') ? menu.css("top", headerHeigh) : menu.css("top", 0);
+    function setTopValue() {
+        header.hasClass('sticky') ? $('body').css("padding-top", headerHeigh + 100) : $('body').css("padding-top", 0);
         menu.css("top", headerHeigh)
     }
 
@@ -87,19 +93,19 @@ $(function () {
             hamburgerIcon.click();
         }
 
-        if ($(this).scrollTop() > headerHeigh) {
+        if ($(this).scrollTop() > 100) {
             if (header.not('.sticky')) {
                 header.addClass('sticky')
                 upperHead.addClass('d-none');
                 calcDimetions();///to handle minue top-remove h top
-                setTopMenu()
+                setTopValue();
             }
         } else {
             if (header.hasClass('sticky')) {
                 header.removeClass('sticky');
                 upperHead.removeClass('d-none');
-                calcDimetions()
-                setTopMenu();
+                calcDimetions();
+                setTopValue();
             }
 
         }
@@ -108,29 +114,205 @@ $(function () {
 
     ///*********checkWindow *************/
     function checkWindow() {
+        let leftOffset = outLContainer.offset().left,//for verical slider
+            righttOffset = outLContainer.offset().reight;
 
-        if (window.innerWidth <= 768) {
+        if (window.innerWidth < 768) {
 
             //  menu.css("left", -menuWidth);
             initiaizeMenu();
             menuWidth = menu.innerWidth();
             calcDimetions();
-            setTopMenu();
+            setTopValue();
             animateMinue();
 
+            outLContainer.css('margin-left', 0);
+            // outLContainer.css('margin-reight', +righttOffset);
 
 
+
+        } else {
+            calcDimetions();
+            let leftOffset = outLContainer.offset().left,//for verical slider
+                righttOffset = outLContainer.offset().reight;
+            outLContainer.css('margin-left', -leftOffset);
+            // outLContainer.css('margin-reight', -leftOffset);
         }
 
     }
-    ////*************************************** */
+    //************End Header******************************************************************************/
 
+    //************Main Content******************************************************************************/
+    let colorSlider = $(".colors-types .slick-slider");
+    ///////////////init slicks////////////////////////////////////////
+    function initColorsSlick() {
+        colorSlider.slick({
+            vertical: true,
+            infinite: true,
+            slidesToShow: 5,
+            slidesToScroll: 1,
+
+            // centerMode: false,
+            prevArrow: ".colors-types .prev-arrow",
+            nextArrow: ".colors-types .next-arrow",
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        vertical: false,
+                        arrows: false,
+                        centerMode: true,
+                        // centerPadding: '40px',
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                },
+                // {
+                //   breakpoint: 480,
+                //   settings: {
+                //     arrows: false,
+                //     centerMode: true,
+                //     centerPadding: '40px',
+                //     slidesToShow: 1
+                //   }
+                // }
+            ]
+
+
+        });
+
+    }
+
+    function initLipsSlick(slickLips) {
+
+        $('.lipsStick .slick-slider').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+
+            dots: true,
+            autoplay: true,
+
+        });
+    }
+
+    /////////////////////////////////////////////////////////////////
+
+    productSelect.text(homeProducts.attr('data-path'));
+    /////////////**view********** */
+
+
+
+
+    function setProdType(lipsobj) {
+        let colors = lipsobj[0].colors,
+            img = lipsobj[1].selectedimg,
+            { src, srcset, name, deg } = img;
+
+
+        colors.map(item => {
+            let { backgroundColor, name, deg } = item;
+            // style="color: red"
+            let style = `background-color:${backgroundColor}`
+            // colorSlider
+
+            let sliderItem = `<div class='slider-item' data-name=${name} data-deg=${deg} id=${name} style=${style}>
+                             <span class="name">${name}</span>
+                             <div class='selected-container'>
+                             <img src="imgs/selected.jpg"
+                             srcset="imgs/selected@2x.jpg 2x,
+                                     imgs/selected@3x.jpg 3x"
+                             class="Selected">
+                              </div>
+                             <span class="deg">${deg}</span>
+                            </div>`;
+
+            colorSlider.append(sliderItem);
+
+        })
+        //this selected
+        let sliderImg = `<div class='slider-item latte-img' data-name=${name} data-deg=${deg} >
+                                <img src=${src}
+                                alt=${name}
+                                srcset=${srcset}/>
+
+                                <div class="latte-desc"> 
+                                    <span class="name">${name}</span>
+                                    <div class='selected-container'>
+                                    <img src="imgs/selected.jpg"
+                                    srcset="imgs/selected@2x.jpg 2x,
+                                            imgs/selected@3x.jpg 3x"
+                                    class="Selected">
+                                     </div>
+                                    <span class="deg">${deg}</span>
+                                </div>
+
+                         </div>`;
+        colorSlider.find('#Truffle').after(sliderImg);
+        initColorsSlick();
+        ///add class selected
+        let colorsItem = $('.colors-types .slider-item');
+        colorsItem.on('click', function () {
+            $(this).toggleClass('select');
+        })
+        //  console.log(colorsItem);
+
+
+    }
+    //////
+    /**
+     * lipsStick">
+                <div class="slick-slider horizantal
+     */
+    function setlipsImgs(lipstickimgs) {
+        let slickLips = $('.lipsStick .slick-slider');
+        lipstickimgs.map(item => {
+            console.log(item);
+            let { src, srcset } = item;
+            let sliderItem = `<div class='slider-item'>
+            <img src=${src}
+            srcset=${srcset}
+           />
+           </div>`;
+            slickLips.append(sliderItem);
+
+        });
+        initLipsSlick(slickLips);
+    }
+    /*ajax req***************************************************************************************/
+    $.ajax({
+        type: "Get",
+        url: "https://my-json-server.typicode.com/sajidaqadomi/productsjson/db",
+        data: "",
+        dataType: 'jsonp',
+        success: function (response) {
+            let lipsobj = response.lips;
+            let lipstickimgs = response.lipstickimgs;
+            console.log(lipstickimgs);
+
+            setProdType(lipsobj);
+            setlipsImgs(lipstickimgs);
+
+        },
+        error: function (xhr, status, error) {
+            console.log(lipstickimgs)
+        }
+
+    });
+
+
+
+
+
+
+
+    ////////////////////////////////////////
     calcDimetions();
     checkWindow();
     $(window).on('resize', function () {
         checkWindow();
-        calcDimetions();
-        setTopMenu();
+        // calcDimetions();
+        setTopValue();
     });
 
 
